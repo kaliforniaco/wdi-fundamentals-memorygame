8c1e5feac9c18c2c57344b720617a0188f94e221
David Epps <kaliforniaco@gmail.com>
2018-06-08 22:15:13 -0600

    added objects to game

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,7 +2,33 @@
 //added for init MASTER
 //console.log("Up and running!");
 
-var cards = ["queen","queen","king","king"];
+var cards = [
+{
+rank:"queen",
+suit:"hearts",
+cardImage:"images/queen-of-hearts.png"
+
+},
+{
+rank:"queen",
+suit:"diamonds",
+cardImage:"images/queen-of-diamonds.png"
+
+},
+{
+rank:"king",
+suit:"hearts",
+cardImage:"images/king-of-hearts.png"
+
+},
+{
+rank:"king",
+suit:"diamonds",
+cardImage:"images/king-of-diamonds.png"
+
+}
+];
+
 var cardsInPlay = [];
 
 var checkForMatch = function () {
@@ -18,9 +44,11 @@ alert("Sorry, try again.");
 
 var flipCard = function (cardID) {
 	
-	console.log ("User flipped " + cards[cardID]);
+	console.log ("User flipped " + cards[cardID].rank);
+	console.log (cards[cardID].cardImage);
+	console.log (cards[cardID].suit);
 
-	cardsInPlay.push (cards[cardID]);
+	cardsInPlay.push (cards[cardID].rank);
 
 	if (cardsInPlay.length === 2) {
 	//console.log(cardsInPlay.length + " Evaluate match!");
